2a948bccf13a962fdc825b02e6caa80f178996ec
Panayiotis Papadopoulos <panos@sfalma.com>
2011-04-25 08:54:14 +0000

    added everyauth

--- app.js
+++ app.js
@@ -3,12 +3,27 @@
  * Module dependencies.
  */
 
-var express = require('express');
+var express = require('express'), 
+    everyauth = require('everyauth'), 
+    app = express.createServer();
+everyauth.helpExpress(app);
 
+var mongoose = require('mongoose').Mongoose;
+var _ = require('underscore')._;
 var app = module.exports = express.createServer();
 
 // Configuration
-
+everyauth.facebook
+  .myHostname('http://cheapcoffee.panosjee.c9.io')
+  .appId('26219543388')
+  .appSecret('e81f33d042406ccdf144a243d9ad1c25')
+  .scope('email')
+  .findOrCreateUser( function (session, accessToken, fbUserMetadata) {
+    // find or create user logic goes here
+    console.log("=================== CONSOLE");
+  })
+  .redirectPath('/');
+  
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -17,6 +32,7 @@ app.configure(function(){
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'slkjfh8u8uiojklsadjflkjiu243rkljdslkfjcheadskjfpjbajdfcof343feendf' }));
   app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
+  app.use(everyauth.middleware());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -37,9 +53,11 @@ app.get('/', function(req, res){
   });
 });
 
+
+
 // Only listen on $ node app.js
 
 if (!module.parent) {
-  app.listen(3000);
+  app.listen(process.env.C9_PORT);
   console.log("Express server listening on port %d", app.address().port);
 }
